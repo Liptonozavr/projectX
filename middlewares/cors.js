7f0b158a0e254af5c8afa579b106a91486d89586
Liptonozavr <149605606+Liptonozavr@users.noreply.github.com>
2024-05-30 21:14:27 +0300

    поменяли cors

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -3,7 +3,7 @@ const allowedCors = [
   "https://students-projects.ru",
   "http://localhost:3000",
   "http://localhost:3001",
-  "https://pindiefront-asket21.nomoredomainswork.ru",
+  "https://domenmomentfront.nomorepartiesco.ru",
 ];
 
 function cors(req, res, next) {
