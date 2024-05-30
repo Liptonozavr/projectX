2c8183cc2848a9d1bfb701b7f3d922f5b1a5f1a5
vm <vm@example.com>
2024-05-30 21:21:45 +0000

    update

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,8 +1,7 @@
 const allowedCors = [
-  "https://practicum.yandex.ru",
-  "https://students-projects.ru",
   "http://localhost:3000",
-  "http://localhost:3001",
+  "https://localhost:3000",
+  "http://domenmomentfront.nomorepartiesco.ru",
   "https://domenmomentfront.nomorepartiesco.ru",
 ];
 
--- models/game.js
+++ models/game.js
@@ -36,19 +36,18 @@ const gameSchema = new mongoose.Schema({
 });
 
 gameSchema.statics.findGameByCategory = function(category) {
-
-  return this.find({}) // Выполним поиск всех игр
-  .populate({
-    path: "categories",
-    match: { name: category } // Опция поможет сопоставить подходящие игры по выбранной категории 
-  })
-  .populate({
-    path: "users",
-    select: "-password" // Позволяет получить записи о пользователях за исключением их паролей (они же хранятся в зашифрованном виде)
-  })
-//   .then(games => {
-//     return games.filter(game => game.catgories.length > 0);
-// });
+  return this.find({})
+    .populate({
+      path: "categories",
+      match: { name: category } 
+    })
+    .populate({
+      path: "users",
+      select: "-password"
+    })
+    .then(games => {
+      return games.filter(game => game.categories.length > 0);
+    });
 };
 
 
